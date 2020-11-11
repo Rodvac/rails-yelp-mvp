e3eca52a16c49f2ffd59d7e220b8cfd2850cdd87
Rodolphe Vacher <rodolphe.vacher@edu.emlyon.com>
2020-11-11 15:45:02 +0100

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:destroy]
 end
+
+# GET /restaurants/restaurant_id/reviews/new
+# POST /restaurants/restaurant_id/reviews
---
+++ db/migrate/20201111111829_change_type_phone_number_to_restaurants.rb
@@ -0,0 +1,5 @@
+class ChangeTypePhoneNumberToRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    change_column :restaurants, :phone_number, :string
+  end
+end
